823e64e074af9b3cfe5d34f211a377730a6e110e
ABHI <abhilashbairi9908@gmail.com>
2022-03-05 22:24:59 +0530

    message

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen/SignInScreen';
